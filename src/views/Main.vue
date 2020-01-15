47e2b1ab23d060628b6b141752b68cb0557e995d
Sky <1919230073@qq.com>
2020-01-15 11:31:03 +0800

    更新用户名

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,15 @@
 <template>
   <el-container>
     <el-header style="height: 8vh">
-      <p>雨果后台管理中心</p>
+      <el-row type="flex" justify="space-between">
+        <el-col :span="12" class="mainTitle">
+          <p>雨果后台管理中心</p>
+        </el-col>
+        <el-col :span="12" class="mainContent">
+          <el-image :src="require('../images/img.jpg')" class="headerImg"></el-image>
+          <span>{{loginUser.username}}</span>
+        </el-col>
+      </el-row>
     </el-header>
     <el-container class="content">
       <el-aside width="200px">
@@ -14,9 +22,25 @@
 </template>
 <script>
 import Menu from "@/views/Menu.vue";
+import { BASEURL } from "../api/base";
 export default {
-  components:{
+  data() {
+    return {
+      loginUser: {
+        username: null,
+        haederimg: null
+      },
+      base_user: BASEURL
+    };
+  },
+  components: {
     Menu
+  },
+  created() {
+    let userObj = this.$store.getters.getUser;
+    this.loginUser.username = userObj.username;
+    this.loginUser.haederimg = userObj.headerimg;
+    console.log(this.$store.getters.getUser);
   }
 };
 </script>
@@ -25,6 +49,21 @@ export default {
   width: 100%;
 
   background: rgb(226, 134, 134);
+}
+.headerImg {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+}
+.mainTitle {
+  text-align: left;
+}
+.mainContent {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  /* border: 1px solid rebeccapurple */
 }
 * {
   margin: 0;
